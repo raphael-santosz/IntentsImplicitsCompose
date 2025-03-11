29f641a3b3197ba668afac72fb6900983072523e
d4089663 <msendinv@gmail.com>
2025-03-12 00:10:37 +0100

    Redefining device capabilitites

--- app/src/main/java/com/example/intentsimplicitscompose/MainActivity.kt
+++ app/src/main/java/com/example/intentsimplicitscompose/MainActivity.kt
@@ -6,29 +6,22 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -36,16 +29,14 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 import com.example.intentsimplicitscompose.ui.theme.IntentsImplicitsComposeTheme
 
 class MainActivity : ComponentActivity() {
 
     private var callPhonePerm = mutableStateOf(false)
 
-    private val permissionCall = arrayOf(
-        android.Manifest.permission.CALL_PHONE,
-    )
+    private val permissionCallPhone = android.Manifest.permission.CALL_PHONE
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,12 +50,9 @@ class MainActivity : ComponentActivity() {
 
     override fun onResume() {
         super.onResume()
-        if (permissionCall.all {
-                ContextCompat.checkSelfPermission(
-                    this,
-                    it
-                ) == PackageManager.PERMISSION_GRANTED
-            }) {
+        if (checkSelfPermission(android.Manifest.permission.CALL_PHONE) ==
+            PackageManager.PERMISSION_GRANTED
+        ) {
             callPhonePerm.value = true
         }
     }
@@ -73,11 +61,10 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun MyApp(modifier: Modifier = Modifier) {
         val context = LocalContext.current
-        val launchCallPerm =
-            rememberLauncherForActivityResult(ActivityResultContracts.RequestMultiplePermissions())
-            { permissionMaps ->
-                val areGranted = permissionMaps.values.reduce { acc, next -> acc && next }
-                if (areGranted) {
+        val launchCallPhonePerm =
+            rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission())
+            { isGranted: Boolean ->
+                if (isGranted) {
                     Toast.makeText(context,"Permission Granted", Toast.LENGTH_SHORT).show()
                     callPhonePerm.value = true
                 } else {
@@ -87,16 +74,13 @@ class MainActivity : ComponentActivity() {
             }
 
         LaunchedEffect(Unit) {
-            if (!permissionCall.all {
-                    ContextCompat.checkSelfPermission(
-                        context,
-                        it
-                    ) == PackageManager.PERMISSION_GRANTED
-                }) {
+            if (checkSelfPermission(android.Manifest.permission.CALL_PHONE) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {callPhonePerm.value = true}
+            else {
                 Log.i(TAG,"Requesting permission")
-                launchCallPerm.launch(permissionCall)
+                launchCallPhonePerm.launch(permissionCallPhone)
             }
-            else {callPhonePerm.value = true}
         }
 
         Surface(modifier = modifier)
@@ -208,29 +192,12 @@ class MainActivity : ComponentActivity() {
                         ).show()
                         val intent = Intent(
                             Intent.ACTION_CALL,
-                            Uri.parse("tel:" + "666666666")
+                            Uri.parse("tel:" + telef)
                         )
                         context.startActivity(intent)
                     })
                 {
                     Text(text = stringResource(R.string.Boton5))
-                }
-
-                ElevatedButton(
-                    modifier = modifier.padding(vertical = 30.dp).padding(vertical = 4.dp),
-                    elevation = ButtonDefaults.elevatedButtonElevation(5.dp),
-                    onClick = {
-                        Toast.makeText(
-                            context,
-                            "Accediendo a contactos",
-                            Toast.LENGTH_LONG
-                        ).show()
-                        val intent = Intent(Intent.ACTION_VIEW)
-                        intent.data = ContactsContract.Contacts.CONTENT_URI
-                        context.startActivity(intent)
-                    })
-                {
-                    Text(text = stringResource(R.string.Boton6))
                 }
             }
         }
@@ -242,6 +209,7 @@ class MainActivity : ComponentActivity() {
         const val address = "Carrer de Jaume II, 69, Lleida"
         const val webEPS = "http://www.eps.udl.cat/"
         const val textoABuscar = "escola politecnica superior UdL"
+        const val telef = "666666666"
     }
 
 
